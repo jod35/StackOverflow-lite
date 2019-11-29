1e25456f09e9c62cb7b0148dc246ef9521b2b341
jod35 <jodestrevin@gmail.com>
2019-11-29 13:57:22 +0300

    account creation works

--- simple/models.py
+++ simple/models.py
@@ -1,6 +1,7 @@
-from . import db
+from . import db,login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     id=db.Column(db.Integer(),primary_key=True)
     username=db.Column(db.String(25),nullable=False,unique=True)
     email=db.Column(db.String(80),nullable=False,unique=True)
@@ -8,3 +9,8 @@ class User(db.Model):
     
     def __repr__(self):
         return "user {}".format(self.username)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
--- simple/views.py
+++ simple/views.py
@@ -1,4 +1,5 @@
-from . import app,db
+from . import app,db,bcrypt
+from flask_login import login_user,logout_user,current_user
 from flask import render_template,redirect,url_for,request,flash
 from .models import User
 
@@ -8,41 +9,29 @@ def index():
 
 @app.route('/signup',methods=['GET', 'POST'])
 def signup():
-    
-
     if request.method == 'POST':
         username=request.form['username']
         email=request.form['email']
         password=request.form['password']
         confirm=request.form['confirm']
 
-        user=User.query.filter_by(username=username).first()
-
-        if user:
-            flash("Account with email {} exists".format(user.email))
-            return redirect(url_for('signup'))
-        
-        else:
-            new_user=User(
-                username=username,
-                email=email,
-                password=password,
-            )
-
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            flash("Account Created Successfully, You can Login")
-
-        
-
-
-
-    
-
-    
+        new_user=User(
+            username=username,
+            email=email,
+            password=bcrypt.generate_password_hash(password)
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Account Created Successfully!!")
+        return redirect('signup')
     return render_template('sign.html')
 
-@app.route('/login')
+@app.route('/login',methods=['GET', 'POST'])
 def login():
+    email=request.form.get('prompt')
+    user=User.query.filter_by(email=email).first()
+    password=request.form.get('password')
+    if user and bcrypt.check_password_hash(password,user.password):
+        login_user(user)
+        return "Logged In"
     return render_template('login.html')
